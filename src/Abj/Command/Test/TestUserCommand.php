171be10b020b78a7c4c40e2bf62bcf8c5f93d5d7
jakabadambalazs <jakabadambalazs@gmail.com>
2018-04-06 11:51:58 +0200

    removing sensitive data

--- src/Abj/Command/Test/TestUserCommand.php
+++ src/Abj/Command/Test/TestUserCommand.php
@@ -10,6 +10,7 @@ namespace Abj\Command\Test;
 use Abj\Console\CommandInterface;
 use Abj\Console\UserAwareCommand;
 use Abj\Logger\ConsoleLogger;
+use dawguk\GarminConnect;
 
 /**
  * Class TestUserCommand
@@ -27,10 +28,34 @@ class TestUserCommand extends UserAwareCommand implements CommandInterface
   protected function executeCommand()
   {
     ConsoleLogger::log("USER TEST COMMAND OK");
-    ConsoleLogger::log($this->userConfiguration);
-
-
+    //ConsoleLogger::log($this->userConfiguration);
+    $this->test1();
 
     //https://github.com/cpfair/tapiriik/blob/master/tapiriik/services/TrainingPeaks/trainingpeaks.py
+  }
+
+  /**
+   * @throws \Exception
+   */
+  protected function test1()
+  {
+    $credentials = [
+      'username' => $this->userConfiguration->get('platform.garminconnect.username'),
+      'password' => $this->userConfiguration->get('platform.garminconnect.password'),
+    ];
+
+    try {
+      $GC = new GarminConnect($credentials);
+      //$activities = $GC->getActivityList();
+    } catch (\Exception $e) {
+      ConsoleLogger::log("Oops: " . $e->getMessage());
+    }
+
+    if(isset($activities)) {
+      print_r($activities);
+    }
+
+
+
   }
 }
